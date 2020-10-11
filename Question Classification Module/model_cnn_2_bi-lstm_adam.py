c1e27b93d1123916669d25459d6b92b9cbcb7b44
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-10-11 21:28:34 +0000

    Updated spirv-cross.

--- Question Classification Module/model_cnn_2_bi-lstm_adam.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam.py
@@ -29,4 +29,5 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-                    validation_da
+                    validation_data=(validation_padded,valid_labels),
+      

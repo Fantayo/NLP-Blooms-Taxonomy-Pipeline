efcad1d3d6e614ddc672a90c07699428a2cf7898
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-10-14 22:20:40 +0000

    save code

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_100-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_100-D.py
@@ -27,3 +27,40 @@ f.close()
 
 print('Found %s word vectors.' % len(embeddings_index))
 print(word_index)
+
+embedding_dim = 100
+max_words = vocab_size              
+
+#Preparing Word Embedding Matrix with GloVe 100-D :
+embedding_matrix = np.zeros((max_words, embedding_dim))
+
+for word, i in word_index.items():
+  if i < max_words:
+
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+
+tf.keras.backend.clear_session()
+optim=tf.keras.optimizers.Adam(clipnorm=1.25)
+model=Sequential()
+model.add(Embedding(vocab_size,100,weights=[embedding_matrix],input_length=train_padded.shape[1]))
+
+model.add(Conv1D(256,3,activation='relu',padding='valid'))
+model.add(MaxPooling1D(pool_size=2))
+
+model.add(Bidirectional(LSTM(256,return_sequences=True)))
+model.add(Bidirectional(LSTM(128)))
+
+
+model.add(Dense(6,activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['accuracy'])
+
+epochs = 30
+batch_size = 8
+model.summary()
+
+
+history = model.fit(train_padded, train_labels, shuffle=True ,

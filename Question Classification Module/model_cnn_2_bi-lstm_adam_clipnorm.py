e2bad47e6626130e146a6dfaf43d53730e2cd4b6
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-10-14 19:02:45 +0000

    Fix serialization

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm.py
@@ -1,3 +1,4 @@
+
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
@@ -9,6 +10,7 @@ embedding_dim=128
 max_length=40
 
 tf.keras.backend.clear_session()
+optim=tf.keras.optimizers.Adam(clipnorm=1.25)
 model=Sequential()
 model.add(Embedding(vocab_size,256,input_length=train_padded.shape[1]))
 
@@ -20,7 +22,7 @@ model.add(Bidirectional(LSTM(128)))
 
 
 model.add(Dense(6,activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['accuracy'])
 
 epochs = 30
 batch_size = 8
@@ -30,8 +32,3 @@ model.summary()
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
                     validation_data=(validation_padded,valid_labels),
-                    callbacks=[EarlyStopping(monitor='val_accuracy',patience=5)],verbose=1)
-
-# Results For This Model :
-#Accuracy -> 80.00 % 
-#Loss ->  1.50

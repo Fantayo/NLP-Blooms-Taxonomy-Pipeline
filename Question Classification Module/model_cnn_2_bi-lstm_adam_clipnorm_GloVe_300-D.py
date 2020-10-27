010ba75094290773f4843ae46ce191bd1fdee226
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-10-27 07:21:24 +0000

    Fix documentation typo.

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_300-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_300-D.py
@@ -12,4 +12,7 @@ path='PATH TO GLOVE EMBEDDINGS'
 #Using Word Embeddings :
 word_index = tokenizer.word_index
 glove_dir = 'path/Glove Embeddings/glove.6B.300d.txt'
-embeddings
+embeddings_index = {}
+
+
+f = open('path/Glove Embed

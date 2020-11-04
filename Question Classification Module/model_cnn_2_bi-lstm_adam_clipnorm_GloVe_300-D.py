35a3d02dd08d7c587b31d624bdfe57daf77854a2
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-11-04 22:54:16 +0000

    1.2.0 release

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_300-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_GloVe_300-D.py
@@ -17,4 +17,6 @@ embeddings_index = {}
 
 f = open('path/Glove Embeddings/glove.6B.300d.txt')
 for line in f:
-    values = line.split
+    values = line.split()
+    word = values[0]
+    coefs = np.

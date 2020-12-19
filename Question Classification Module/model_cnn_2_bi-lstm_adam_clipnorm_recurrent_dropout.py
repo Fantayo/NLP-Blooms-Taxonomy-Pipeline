a103d42c1a4000d5dee10201505f137e4466683f
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-12-19 15:31:03 +0000

    fix namespace

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_recurrent_dropout.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_recurrent_dropout.py
@@ -1,3 +1,4 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,M
+from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
+from tensorflow.keras.callbac

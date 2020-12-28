4ea939c577c0452ca345a9faf8977712e0c3346f
Fantayo <125394203+Fantayo@users.noreply.github.com>
2020-12-28 19:54:11 +0000

    fix sonar

--- Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_recurrent_dropout.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_clipnorm_recurrent_dropout.py
@@ -14,4 +14,7 @@ model=Sequential()
 model.add(Embedding(vocab_size,256,input_length=train_padded.shape[1]))
 
 model.add(Conv1D(256,3,activation='relu',padding='valid'))
-model.add(M
+model.add(MaxPooling1D(pool_size=2))
+
+
+model.add(B

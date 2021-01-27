36190dae2825808e2465238bfd95e4084f865a8c
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-01-27 15:19:21 +0000

    added localization

--- Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
-from tensorflow.keras.callbacks import EarlySt
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+vocab_size=10000
+embedding_d

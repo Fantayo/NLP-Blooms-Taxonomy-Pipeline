26917cd23ded66d144c41763dfa41f46d36f48e2
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-03-03 06:06:16 +0000

    fix test build

--- Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
@@ -31,4 +31,5 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-                    validation_dat
+                    validation_data=(validation_padded,valid_labels),
+            

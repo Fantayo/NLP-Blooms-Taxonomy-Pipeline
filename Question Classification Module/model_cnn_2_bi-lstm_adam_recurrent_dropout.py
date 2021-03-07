a59e749bf2b87a164ce29d5ef9c950b961f18680
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-03-07 12:57:14 +0000

    * whitespaces

--- Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
+++ Question Classification Module/model_cnn_2_bi-lstm_adam_recurrent_dropout.py
@@ -32,4 +32,8 @@ model.summary()
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
                     validation_data=(validation_padded,valid_labels),
-                    callbacks=[EarlyStopping(monitor='val_accuracy',patience=5)],verbos
+                    callbacks=[EarlyStopping(monitor='val_accuracy',patience=5)],verbose=1)
+
+
+
+# Results For Th

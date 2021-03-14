5449c7104f82a3e8ed2f1cbde3035b2b745efb67
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-03-14 15:23:20 +0000

    charger-manager: Ensure event is not used as format string

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,Gl
+from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,F

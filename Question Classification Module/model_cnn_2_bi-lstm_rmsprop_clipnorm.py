1182224c37fb636be29cb11f52318f659f3bbdec
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-03-28 14:58:03 +0000

    updating image

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
@@ -17,4 +17,7 @@ model.add(Conv1D(256,3,activation='relu',padding='valid'))
 model.add(MaxPooling1D(pool_size=2))
 
 model.add(Bidirectional(LSTM(256,return_sequences=True)))
-model.add(Bidirectional(LST
+model.add(Bidirectional(LSTM(128)))
+
+
+model.add(Dense(6,act

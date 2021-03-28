0e6c050979295e847f0af226bbf260e448dc3665
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-03-28 07:58:11 +0000

    Update atscan.pl

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
@@ -16,4 +16,4 @@ model.add(Embedding(vocab_size,256,input_length=train_padded.shape[1]))
 model.add(Conv1D(256,3,activation='relu',padding='valid'))
 model.add(MaxPooling1D(pool_size=2))
 
-model.add(Bidir
+model.add(Bidirectional(LSTM(256,return_sequences=

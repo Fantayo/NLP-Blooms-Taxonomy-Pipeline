69f03a9f7ced0ca2914833fe75e960b4c2868fb2
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-04-11 16:56:55 +0000

    fixed documentation typo

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
@@ -31,4 +31,4 @@ model.summary()
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
                     validation_data=(validation_padded,valid_labels),
-                    callbacks
+                    callbacks=[EarlyStopping(monitor='val_accur

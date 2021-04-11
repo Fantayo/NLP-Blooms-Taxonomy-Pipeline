b6e6d41105399659fb1effa862832e2d87d27709
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-04-11 08:43:42 +0000

    Wording updates

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm.py
@@ -30,4 +30,5 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-                    validation_data=(validation_padded,valid_labe
+                    validation_data=(validation_padded,valid_labels),
+                    callbacks

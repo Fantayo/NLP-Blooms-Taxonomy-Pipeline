65e0fdb8e30215fb7ab011dd1dfbf1bff3a9b5cb
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-05-02 15:16:40 +0000

    net: calxedaxgmac: throw away overrun frames

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
-from tensorflow.kera
+from tensorflow.keras.callbacks import EarlyStopp

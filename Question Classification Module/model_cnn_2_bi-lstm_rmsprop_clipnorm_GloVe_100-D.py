b09ef0a2e8e5e5f62cfaf9ab4c05792f24a4dc5c
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-08-19 18:49:59 +0000

    Revert "Initial Commit"

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
@@ -39,4 +39,4 @@ for word, i in word_index.items():
     embedding_vector = embeddings_index.get(word)
     if embedding_vector is not None:
         # words not found in embedding index will be all-zeros.
-        embeddin
+        embedding_matrix[i] = embedding_vector

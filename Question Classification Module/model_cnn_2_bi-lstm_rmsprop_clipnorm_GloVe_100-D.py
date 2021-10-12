76b084a75e736d8e735fa96e5bdcb5f8def6f8e3
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-10-12 08:07:34 +0000

    edit index

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
@@ -64,4 +64,5 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-                    validation_data=(valid
+                    validation_data=(validation_padded,valid_labels),
+             

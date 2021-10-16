70a8db356528e759630017d7f86b17b9c5b65ab0
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-10-16 13:12:24 +0000

    udf: avoid info leak on export

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_100-D.py
@@ -75,4 +75,4 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-        
+                    validation_data=(validation_pa

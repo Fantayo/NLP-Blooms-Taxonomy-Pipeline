c6ec098d919eb434f8255b74bcf909b72d423b83
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-11-23 14:38:10 +0000

    Add bower file

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_300-D .py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_300-D .py
@@ -47,4 +47,5 @@ optim=tf.keras.optimizers.RMSprop(clipnorm=1.25)
 model=Sequential()
 model.add(Embedding(vocab_size,300,weights=[embedding_matrix2],input_length=train_padded.shape[1]))
 
-model.add(Conv1D(256,3,activation='relu',pad
+model.add(Conv1D(256,3,activation='relu',padding='valid'))
+model.add(MaxPool

4444b94df2847b880e657812c0ea24cfde170870
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-11-28 18:50:10 +0000

    - Added mat-breadcrumb - Added xp-finder-behavior - Major documentation improvements - Minor bug fixes - Moved item finding from xp-selected-behavior to xp-finder-behavior

--- Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_300-D .py
+++ Question Classification Module/model_cnn_2_bi-lstm_rmsprop_clipnorm_GloVe_300-D .py
@@ -62,4 +62,4 @@ model.summary()
 
 history = model.fit(train_padded, train_labels, shuffle=True ,
                     epochs=epochs, batch_size=batch_size, 
-  
+                    validation_data=(valida

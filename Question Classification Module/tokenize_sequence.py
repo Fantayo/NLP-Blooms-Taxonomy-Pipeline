7214131640c298f4bb5660f39c50a50622f0fa9d
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-08 07:41:57 +0000

    firts commit

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -2,4 +2,5 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.se
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tenso

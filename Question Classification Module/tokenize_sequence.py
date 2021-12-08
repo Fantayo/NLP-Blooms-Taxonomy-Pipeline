bf46d4290441f31be302ccc021414e785bc3aeba
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-08 11:39:28 +0000

    Fix type error.

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -3,4 +3,5 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tenso
+from tensorflow.keras import Sequential
+fr

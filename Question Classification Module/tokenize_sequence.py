a023fb0c9b7a6b5f600048687fa934a1013be6e1
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-14 10:59:01 +0000

    Fix failing test.

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense,Conv1D,L
+from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPoolin

6b4ae870e9a40e2eec9aa09526b53e156462997d
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-19 19:22:49 +0000

    extending documentation

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -5,4 +5,5 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
-from tensorflow.keras.callbacks import Red
+from tensorflow.keras.callbacks import ReduceLROnPlateau,EarlyStopping
+

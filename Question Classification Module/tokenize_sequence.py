df3d7dc73443a10e54290a77a940f09e7f07c346
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-19 12:21:37 +0000

    yj

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -4,4 +4,5 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,M
+from tensorflow.keras.layers import Dense,Conv1D,LSTM,Bidirectional,Embedding,GlobalMaxPooling1D,Dropout,Flatten,MaxPool1D,MaxPooling1D
+from tensorflow.keras.callbacks import Red

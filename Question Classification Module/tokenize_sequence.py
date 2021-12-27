32e4bb40bab8ac2ca049586969113af7c5146f6a
Fantayo <125394203+Fantayo@users.noreply.github.com>
2021-12-27 05:37:01 +0000

    Bumped year

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -11,4 +11,4 @@ from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-f
+from nltk.stem import PorterStemmer

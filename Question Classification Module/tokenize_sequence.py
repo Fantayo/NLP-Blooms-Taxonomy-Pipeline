f2d246e922d889dc55b3b74a29df9799f3b563e3
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-01-12 15:30:54 +0000

    v2.11.0

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -33,4 +33,4 @@ len(word_index)
 train_seq=tokenizer.texts_to_sequences(X_train)
 train_padded=pad_sequences(train_seq,maxlen=max_length,padding=padding_type,truncating=trunc_type)
 
-vali
+validation_seq=tokenizer.texts_to_sequences(

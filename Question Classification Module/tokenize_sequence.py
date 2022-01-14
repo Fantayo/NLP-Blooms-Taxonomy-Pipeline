28cefa82a4a0a49457e63d5eb09397eb8d412955
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-01-14 20:36:53 +0000

    Update results.html

--- Question Classification Module/tokenize_sequence.py
+++ Question Classification Module/tokenize_sequence.py
@@ -34,4 +34,4 @@ train_seq=tokenizer.texts_to_sequences(X_train)
 train_padded=pad_sequences(train_seq,maxlen=max_length,padding=padding_type,truncating=trunc_type)
 
 validation_seq=tokenizer.texts_to_sequences(y_train)
-validation_padded=pad_sequences(validation_seq,maxlen=max_l
+validation_padded=pad_sequences(validation_seq,maxlen=max_length,padding=padding_type,truncating=

6cc27aa3bc8217ecadf453a44bd260a0b874bd49
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-02-09 13:19:57 +0000

    Initial project skeleton

--- Semantic Similairty with Siamese Neural Network - Case Study Module/create_embeddings.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/create_embeddings.py
@@ -18,4 +18,5 @@ f = open('/path/Glove Embeddings/glove.6B.100d.txt')
 for line in f:
     values = line.split()
     word = values[0]
-    coefs = np.asarray(values[1:], dtype=
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index

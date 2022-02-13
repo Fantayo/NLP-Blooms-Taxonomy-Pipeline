4a7a1216b542940d4ef19506b3eb75dd1c62e840
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-02-13 15:37:01 +0000

    fix documentation typo

--- Semantic Similairty with Siamese Neural Network - Case Study Module/create_embeddings.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/create_embeddings.py
@@ -33,4 +33,5 @@ embedding_matrix = np.zeros((max_words, embedding_dim))
 for word, i in word_index.items():
   if i < max_words:
 
-    embedding_vec
+    embedding_vector = embeddings_index.get(word)
+    if e

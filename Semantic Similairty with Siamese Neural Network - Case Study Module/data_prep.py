018bfc91f35ca34d92e0b843924ac32c75bae2b3
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-04-19 05:49:27 +0000

    stats fix

--- Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
@@ -41,4 +41,5 @@ def process_text(text):
    text = re.sub(r"don\'t","do not",str(text))
    text = re.sub(r"he\'s","he is",str(text)) 
    text = re.sub(r"there\'s","there is",str(text)) 
-   text = re.sub(r"that\'s","that
+   text = re.sub(r"that\'s","that is",str(text)) 
+   text = re.sub(r"can

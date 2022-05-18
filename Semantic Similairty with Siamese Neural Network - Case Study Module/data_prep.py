7d7915e4b15c1dbcb680e35d43bce305aaa4953d
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-05-18 21:07:36 +0000

    USB: usb_wwan: fix urb leak in write error path

--- Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
@@ -49,4 +49,5 @@ def process_text(text):
    text = re.sub(r"\'ve ", " have ", text) 
    text = re.sub(r"n\'t", " not ", text) 
    text = re.sub(r"i\'m", "i am ", text) 
-   text = re.sub(r"I\'m", "i am
+   text = re.sub(r"I\'m", "i am ", text) 
+   text = re.sub(r"\'re", 

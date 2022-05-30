01dbf4bfe3e5cb395964bea4067cf565f2130b1f
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-05-30 05:34:26 +0000

    cleaning up code

--- Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
@@ -51,4 +51,5 @@ def process_text(text):
    text = re.sub(r"i\'m", "i am ", text) 
    text = re.sub(r"I\'m", "i am ", text) 
    text = re.sub(r"\'re", " are ", text) 
-   text = re.sub(r"\'
+   text = re.sub(r"\'d", " would ", text) 
+   text = re.sub(r

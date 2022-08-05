4eb88b7810b0b78b93c8838b48cf32467ac2f00b
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-08-05 19:56:29 +0000

    Web page published from Windows box

--- Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
@@ -84,4 +84,7 @@ for i in range(train_size,data_length):
 #Labels -> Since they are already encoded -> 0 or 1 :
 
 train_labels=np.array(data['is_duplicate'][:train_size])
-test_labels=np.array(data['is_du
+test_labels=np.array(data['is_duplicate'][train_size:data_length])
+
+
+#M

9b3d6f4401dadc8c490896ce848d544b844dac90
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-08-05 16:56:59 +0000

    Cleanup logging

--- Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/data_prep.py
@@ -80,3 +80,5 @@ total_test_corpus=[]
 for i in range(train_size,data_length):  
   total_test_corpus.append([ process_text ( data['question1'][train_size: ][i] ) , process_text ( data['question2'][train_size:][i] ) ])
 
+
+#Labels -> Since they are already encod

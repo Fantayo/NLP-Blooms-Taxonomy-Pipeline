7eebb36453da2e10153b0c887b3a2803c07ba01a
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-08-30 18:41:12 +0000

    Update lock files

--- Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
@@ -2,4 +2,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
-import tensor
+import tensorflow.keras.backend as K
+
+
+from

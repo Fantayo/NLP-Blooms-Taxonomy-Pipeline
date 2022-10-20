79086d802461d752e40f3efd59e8a6d8ba183c65
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-10-20 12:16:24 +0000

    Python 3 support

--- Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
@@ -28,4 +28,4 @@ def siamese_manhattan_network(embed_mat_weights):
 	ques2 = Input(shape=(maxsentence_length,))
 
 	embedding_layer = Embedding(input_dim=max_num_words,output_dim=embedding_dim,weights=[embed_mat_weights],
-   
+    		trainable=False,input_leng

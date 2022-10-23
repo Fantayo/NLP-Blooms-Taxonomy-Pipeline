caa7d86efb9833360e32fcfb33cb92f47fb1267b
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-10-23 10:37:06 +0000

    obsolete filters

--- Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
@@ -31,4 +31,4 @@ def siamese_manhattan_network(embed_mat_weights):
     		trainable=False,input_length=maxsentence_length)
 
 	ques1_embed = embedding_layer(ques1)
-	ques
+	ques2_embed = embedding_layer(ques2

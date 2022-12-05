766b9c1f8501d6fc2c2810aa99f98fb867e41593
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-12-05 18:32:35 +0000

    silence warnings

--- Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/model.py
@@ -59,4 +59,4 @@ start = time.perf_counter()
 
 history=model.fit([train_pad1,train_pad2],train_labels,
           validation_data=([test_pad1,test_pad2],test_labels),
-          batch_size=
+          batch_size=batchsize,epochs=epochs,verbos

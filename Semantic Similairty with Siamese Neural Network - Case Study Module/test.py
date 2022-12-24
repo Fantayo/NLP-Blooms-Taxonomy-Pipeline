14ee37bab6c01ce425c112bcbbd28669ff3df68d
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-12-24 20:30:25 +0000

    cherrypicking from my development branch

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -2,4 +2,5 @@
 def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
 
 
-  test_sentences1 = [x[0].lower
+  test_sentences1 = [x[0].lower() for x in test_sentences_pair]
+  test_sente

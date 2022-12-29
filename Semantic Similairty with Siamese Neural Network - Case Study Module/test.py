2f1b42f12365fdda2c613db4e24a1aa9bbd94023
Fantayo <125394203+Fantayo@users.noreply.github.com>
2022-12-29 20:17:05 +0000

    Sloader update on _data/Bulma.json

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -3,4 +3,4 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
 
 
   test_sentences1 = [x[0].lower() for x in test_sentences_pair]
-  test_sente
+  test_sentences2 = [x[1].lower() for x in tes

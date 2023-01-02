1c9c495bf1ad33e9c1f209dd6f946be5a401d0d9
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-02 18:29:20 +0000

    update datasets

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -10,4 +10,4 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
 
 
   test_data_1 = pad_sequences(test_sequences_1, maxlen=max_sequence_length)
-  te
+  test_data_2 = pad_sequences(test_sequenc

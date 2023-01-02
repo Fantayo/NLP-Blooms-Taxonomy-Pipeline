3a24a522da47c3aa0dca0a282929dfe2ad4bd78b
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-02 15:17:04 +0000

    sysrq: Emergency Remount R/O in reverse order

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -6,4 +6,5 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
   test_sentences2 = [x[1].lower() for x in test_sentences_pair]
 
   test_sequences_1 = tokenizer.texts_to_sequences(test_sentences1)
-  test_sequences_2 = token
+  test_sequences_2 = tokenizer.texts_to_sequences(test_sentences2)
+

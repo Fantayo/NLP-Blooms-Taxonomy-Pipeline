4daf6e00082c92657835a4c1cdb60a2b065c247e
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-02 10:43:24 +0000

    chore(package): update eslint to version 2.11.1

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -5,4 +5,5 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
   test_sentences1 = [x[0].lower() for x in test_sentences_pair]
   test_sentences2 = [x[1].lower() for x in test_sentences_pair]
 
-  test_sequences_1 = tokenizer.texts_to_sequences(test_sent
+  test_sequences_1 = tokenizer.texts_to_sequences(test_sentences1)
+  test_sequences_2 = token

799c5d132715474595ae034380977980119969bb
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-02 07:07:28 +0000

    Updating comments

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -3,4 +3,6 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
 
 
   test_sentences1 = [x[0].lower() for x in test_sentences_pair]
-  test_sentences2 = [x[1].lower() for x in tes
+  test_sentences2 = [x[1].lower() for x in test_sentences_pair]
+
+  test_sequences_1 = t

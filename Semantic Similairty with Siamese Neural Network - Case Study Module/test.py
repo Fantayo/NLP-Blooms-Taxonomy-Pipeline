d4c4d386a3950f77389f2bc3d49d4e9d8bb048e3
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-02 07:50:15 +0000

    Revert "Create README.md"

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -5,4 +5,4 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
   test_sentences1 = [x[0].lower() for x in test_sentences_pair]
   test_sentences2 = [x[1].lower() for x in test_sentences_pair]
 
-  test_sequences_1 = t
+  test_sequences_1 = tokenizer.texts_to_sequences(test_sent

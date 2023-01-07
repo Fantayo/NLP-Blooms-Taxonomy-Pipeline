239493e84bad0c11d801456e4fa925f1aa9d7dd1
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-01-07 12:48:39 +0000

    Setting version to 2.0.0-SNAPSHOT

--- Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/test.py
@@ -16,4 +16,4 @@ def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
 
 def get_predictions(test_data_1,test_data_2,model):
   preds = list(model.predict([test_data_1, test_data_2], verbose=1).ravel())
-  results = [(x, y) for
+  results = [(x, y) for (x, y) in zip(test_sentence_pairs, p

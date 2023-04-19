9dc36458b2781f79d640cd27669d45336fe94a15
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-04-19 11:50:33 +0000

    add spec file

--- Semantic Similairty with Siamese Neural Network - Case Study Module/tokenize_sequence.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/tokenize_sequence.py
@@ -2,4 +2,6 @@
 import tensorflow as tf
 import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokeniz
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+#Custom function to obtain the leng

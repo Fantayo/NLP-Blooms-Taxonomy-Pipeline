ff58c37d0be6e30b5a506f6413c4d3b917b04656
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-04-19 12:02:50 +0000

    perf test: fix a build error on builtin-test

--- Semantic Similairty with Siamese Neural Network - Case Study Module/tokenize_sequence.py
+++ Semantic Similairty with Siamese Neural Network - Case Study Module/tokenize_sequence.py
@@ -4,4 +4,5 @@ import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-#Custom function to obtain the leng
+#Custom function to obtain the length of largest string in corpus :
+def get_max_seq_l

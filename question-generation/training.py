fe4183555c1024cfc678f3d36573b806091e8b68
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-06-21 09:43:49 +0000

    Update version.txt for the branch and trunk

--- question-generation/training.py
+++ question-generation/training.py
@@ -12,4 +12,7 @@ import pandas as pd
 import numpy as np
 
 import torch
-from torch.utils.data import Dataset, DataLo
+from torch.utils.data import Dataset, DataLoader
+
+from termcolor import colored
+import tex

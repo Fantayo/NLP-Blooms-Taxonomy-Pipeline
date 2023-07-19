295522dba72cb5819b849eba4d10c325bc81c7e6
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-07-19 16:51:16 +0000

    Version 1.0.10

--- question-generation/training.py
+++ question-generation/training.py
@@ -75,4 +75,4 @@ class QuestionGenerationDataset(Dataset):
 
             passage,answer,target = self.data.loc[idx, self.passage_column],self.data.loc[idx, self.answer], self.data.loc[idx, self.question]
 
-  
+            input_ = "context: %s  answer: %s </s>" %

89f1d1272473125c122df628f16677288a3f4b3a
Fantayo <125394203+Fantayo@users.noreply.github.com>
2023-07-19 18:16:02 +0000

    Fix requirements

--- question-generation/training.py
+++ question-generation/training.py
@@ -75,4 +75,5 @@ class QuestionGenerationDataset(Dataset):
 
             passage,answer,target = self.data.loc[idx, self.passage_column],self.data.loc[idx, self.answer], self.data.loc[idx, self.question]
 
-            input_ = "context: %s  answer: %s </s>" %
+            input_ = "context: %s  answer: %s </s>" % (passage, answer)
+      
